7cd1802d107f324a26a119572c5826cd8a8a33ac
TangliziGit <tanglizimail@foxmail.com>
2020-07-13 16:12:52 +0800

    test SearchRequest

--- src/test/scala/me/tanglizi/se/EngineActorTest.scala
+++ src/test/scala/me/tanglizi/se/EngineActorTest.scala
@@ -1,5 +1,22 @@
 package me.tanglizi.se
 
+import org.junit._
+import Assert._
+import me.tanglizi.se.engine.Engine
+import me.tanglizi.se.entity.Document
+import me.tanglizi.se.entity.Protocol.SearchRequest
+
+@Test
 class EngineActorTest {
 
+  @Test
+  def testSearchRequest(): Unit = {
+    val sentence: String = "我 天安门"
+    val callback: List[Document] => Unit =
+      docs => println(docs)
+
+    Engine.engineActor ! SearchRequest(sentence, callback)
+
+    Thread.sleep(1000)
+  }
 }
